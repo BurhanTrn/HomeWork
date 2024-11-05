b7c27ab8a80498b1abd200b5d70874a7e2700483
BurhanTrn <burhantorunnn@gmail.com>
2024-11-05 15:46:16 +0300

    .

--- HomeWork_11/BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ HomeWork_11/BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852d1b180da01b330511d4334a9134d4afabddf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab00f5455e72dbdb571ee8738068cca406448893")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
